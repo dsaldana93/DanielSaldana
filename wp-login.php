5c8ce2ff7dfe27f925da1bd41dd060f28584ca64
dsaldana93 <dnsaldanagtz@gmail.com>
2022-06-23 14:16:54 -0600

    Cambio de estilo
    Fix bug cambiando el estilo

--- wp-login.php
+++ wp-login.php
@@ -5,11 +5,12 @@
 	<title>Acceder &lsaquo; Daniel Saldana &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="/https://dsaldana93.github.io/DanielSaldana/wp-includes/css/dashicons.min.css?ver=6.0" media="all">
-<link rel="stylesheet" id="buttons-css" href="/https://dsaldana93.github.io/DanielSaldana/wp-includes/css/buttons.min.css?ver=6.0" media="all">
-<link rel="stylesheet" id="forms-css" href="/https://dsaldana93.github.io/DanielSaldana/wp-admin/css/forms.min.css?ver=6.0" media="all">
-<link rel="stylesheet" id="l10n-css" href="/https://dsaldana93.github.io/DanielSaldana/wp-admin/css/l10n.min.css?ver=6.0" media="all">
-<link rel="stylesheet" id="login-css" href="/https://dsaldana93.github.io/DanielSaldana/wp-admin/css/login.min.css?ver=6.0" media="all">
+<link href="https://fonts.gstatic.com/" crossorigin rel="preconnect">
+<link rel="stylesheet" id="dashicons-css" href="https://dsaldana93.github.io/DanielSaldana/wp-includes/css/dashicons.min.css?ver=6.0" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://dsaldana93.github.io/DanielSaldana/wp-includes/css/buttons.min.css?ver=6.0" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://dsaldana93.github.io/DanielSaldana/wp-admin/css/forms.min.css?ver=6.0" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://dsaldana93.github.io/DanielSaldana/wp-admin/css/l10n.min.css?ver=6.0" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://dsaldana93.github.io/DanielSaldana/wp-admin/css/login.min.css?ver=6.0" type="text/css" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
@@ -20,7 +21,7 @@
 		<div id="login">
 		<h1><a href="https://es-mx.wordpress.org/">Funciona gracias a WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://dsaldana93.github.io/DanielSaldana/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Nombre de usuario o dirección de correo</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username">
@@ -38,20 +39,20 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Recuérdame</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Acceder">
-									<input type="hidden" name="redirect_to" value="/wp-admin/">
+									<input type="hidden" name="redirect_to" value="https://dsaldana93.github.io/DanielSaldana/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="/https://dsaldana93.github.io/DanielSaldana/wp-login.php?action=lostpassword">¿Olvidaste tu contraseña?</a>
+								<a href="https://dsaldana93.github.io/DanielSaldana/wp-login.php?action=lostpassword">¿Olvidaste tu contraseña?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="/https://dsaldana93.github.io/DanielSaldana/">&larr; Ir a Daniel Saldana</a>		</p>
+			<a href="https://dsaldana93.github.io/DanielSaldana/">&larr; Ir a Daniel Saldana</a>		</p>
 			</div>
 				<div class="language-switcher">
 				<form id="language-switcher" action="" method="get">
@@ -70,46 +71,54 @@ if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 
 					</form>
 				</div>
-				<script src="/https://dsaldana93.github.io/DanielSaldana/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
-<script src="/https://dsaldana93.github.io/DanielSaldana/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
-<script id="zxcvbn-async-js-extra">
-var _zxcvbnSettings = {"src":"\/wp-includes\/js\/zxcvbn.min.js"};
+				<script type="text/javascript" src="https://dsaldana93.github.io/DanielSaldana/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script type="text/javascript" src="https://dsaldana93.github.io/DanielSaldana/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+<script type="text/javascript" id="zxcvbn-async-js-extra">
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"https:\/\/dsaldana93.github.io\/DanielSaldana\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
 </script>
-<script src="/https://dsaldana93.github.io/DanielSaldana/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script src="/https://dsaldana93.github.io/DanielSaldana/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
-<script src="/https://dsaldana93.github.io/DanielSaldana/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script src="/https://dsaldana93.github.io/DanielSaldana/wp-includes/js/dist/hooks.min.js?ver=c6d64f2cb8f5c6bb49caca37f8828ce3" id="wp-hooks-js"></script>
-<script src="/https://dsaldana93.github.io/DanielSaldana/wp-includes/js/dist/i18n.min.js?ver=ebee46757c6a411e38fd079a7ac71d94" id="wp-i18n-js"></script>
-<script id="wp-i18n-js-after">
+<script type="text/javascript" src="https://dsaldana93.github.io/DanielSaldana/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="https://dsaldana93.github.io/DanielSaldana/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="https://dsaldana93.github.io/DanielSaldana/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="https://dsaldana93.github.io/DanielSaldana/wp-includes/js/dist/hooks.min.js?ver=c6d64f2cb8f5c6bb49caca37f8828ce3" id="wp-hooks-js"></script>
+<script type="text/javascript" src="https://dsaldana93.github.io/DanielSaldana/wp-includes/js/dist/i18n.min.js?ver=ebee46757c6a411e38fd079a7ac71d94" id="wp-i18n-js"></script>
+<script type="text/javascript" id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
-<script id="password-strength-meter-js-extra">
+<script type="text/javascript" id="password-strength-meter-js-extra">
+/* <![CDATA[ */
 var pwsL10n = {"unknown":"Fortaleza de contraseña desconocida","short":"Muy débil","bad":"Débil","good":"Medio","strong":"Fuerte","mismatch":"No coinciden"};
+/* ]]> */
 </script>
-<script id="password-strength-meter-js-translations">
+<script type="text/javascript" id="password-strength-meter-js-translations">
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2022-05-05 23:05:10+0000","generator":"GlotPress\/4.0.0-alpha.1","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"es_MX"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["¡%1$s está en desuso desde la versión %2$s! Usa %3$s en su lugar. Por favor considera escribir un código más inclusivo."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
 </script>
-<script src="/https://dsaldana93.github.io/DanielSaldana/wp-admin/js/password-strength-meter.min.js?ver=6.0" id="password-strength-meter-js"></script>
-<script src="/https://dsaldana93.github.io/DanielSaldana/wp-includes/js/underscore.min.js?ver=1.13.3" id="underscore-js"></script>
-<script id="wp-util-js-extra">
+<script type="text/javascript" src="https://dsaldana93.github.io/DanielSaldana/wp-admin/js/password-strength-meter.min.js?ver=6.0" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://dsaldana93.github.io/DanielSaldana/wp-includes/js/underscore.min.js?ver=1.13.3" id="underscore-js"></script>
+<script type="text/javascript" id="wp-util-js-extra">
+/* <![CDATA[ */
 var _wpUtilSettings = {"ajax":{"url":"\/DanielSaldana\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
 </script>
-<script src="/https://dsaldana93.github.io/DanielSaldana/wp-includes/js/wp-util.min.js?ver=6.0" id="wp-util-js"></script>
-<script id="user-profile-js-extra">
+<script type="text/javascript" src="https://dsaldana93.github.io/DanielSaldana/wp-includes/js/wp-util.min.js?ver=6.0" id="wp-util-js"></script>
+<script type="text/javascript" id="user-profile-js-extra">
+/* <![CDATA[ */
 var userProfileL10n = {"user_id":"0","nonce":"7a6935c966"};
+/* ]]> */
 </script>
-<script id="user-profile-js-translations">
+<script type="text/javascript" id="user-profile-js-translations">
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2022-05-05 23:05:10+0000","generator":"GlotPress\/4.0.0-alpha.1","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"es_MX"},"Your new password has not been saved.":["Tu contraseña no ha sido guardada"],"Show":["Mostrar"],"Hide":["Ocultar"],"Confirm use of weak password":["Confirma el uso de una contraseña débil."],"Hide password":["Ocultar contraseña"],"Show password":["Mostrar contraseña"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
 </script>
-<script src="/https://dsaldana93.github.io/DanielSaldana/wp-admin/js/user-profile.min.js?ver=6.0" id="user-profile-js"></script>
+<script type="text/javascript" src="https://dsaldana93.github.io/DanielSaldana/wp-admin/js/user-profile.min.js?ver=6.0" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
